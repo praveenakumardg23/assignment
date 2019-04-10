82574d69e49e99684ffcd0279ded92b26a4339f3
Praveena Gaonkar <praveenkumardg23@gmail.com>
2019-04-10 15:32:56 +0530

    Update index.js

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,19 +1,23 @@
-import { createStore, combineReducers, applyMiddleware  } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose} from 'redux'
 import AssignmentReducer from '../reducer'
 import { routerMiddleware, push, browserHistory } from 'react-router-redux'
 import thunk from 'redux-thunk';
 import createHistory from 'history/createBrowserHistory'
+import routerHistoryMiddleware from '../navigation/routerMiddleware';
 
 export const history = createHistory()
 
 const middleware = [
     thunk,
+    routerHistoryMiddleware(history),
     routerMiddleware(history)
 ]
 
 const store = createStore(
     AssignmentReducer,
-    applyMiddleware(...middleware)
+    compose(
+        applyMiddleware(...middleware)
+    )
 )
 
 export default store;
